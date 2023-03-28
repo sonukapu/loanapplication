41197680f778b84da4d524ba34278f47f566a759
sonukapu <saurabhmeshram69@gmai.com>
2023-03-28 14:05:07 +0530

    [STNO-01] INTITIAL COMMIT

---
+++ springbootloanapplication/src/main/java/com/csi/SpringbootloanapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootloanapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootloanapplicationApplication.class, args);
+	}
+
+}
